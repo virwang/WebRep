7bdcb281741a8ad0e511cf79290f7c9eb61c3866
mill <vir.wang@hotmail.com>
2020-02-18 21:29:42 +0800

    add id to login_page.js, and check if passwords are the same.

--- javascript/login_page.js
+++ javascript/login_page.js
@@ -1,8 +1,22 @@
+// this is login page js
 var request;
 function sendInfo()
 {
-  var v =document.vinform.account.value;
-  var url="login_page.jsp?val="+v;
+  var account =document.vinform.account.value;
+  var email = document.vinform.email.value;
+  var password1 =document.vinform.password1.value;
+  var password2 =document.vinform.password2.value;
+  var name =document.vinform.name.value;
+  var url="login_page.jsp?val="+account+ "&email="+ "&password1="+ "&password2="+ "&name=";
+
+  // check if password1 == password2?
+  var n = password1.locaLCompare(password2);
+  if(n !==0)
+  {
+    alert("PLEASE INPUT THE SAME PASSWORD!")
+    return;
+  }
+
 
   if(window.XMLHttpRequest) {
     request=new XMLHttpRequest();
